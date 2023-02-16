ec1f94f20b6bd837d3608acc5a87d8b79cbb5beb
Sana Rzgar <sanarizgar34@gmail.com>
2023-02-16 23:22:33 +0300

    add all

--- main.js
+++ main.js
@@ -11,7 +11,10 @@ async function fetchProducts() {
     console.error(`Could not get products: ${error}`);
   }
 }
-
+const cards = document.querySelector(".cards");
+// const nameCard = document.querySelector("name");
+// const statusCard = document.querySelector("status");
+// const genderCard = document.querySelector("gender");
 const promise = fetchProducts();
 promise.then((data) => {
   data.results
@@ -23,5 +26,44 @@ promise.then((data) => {
         item.character = "main";
       }
       console.log(item);
+
+      const card = document.createElement("div");
+      card.classList = "card";
+      cards.appendChild(card);
+
+      const cardImg = document.createElement("img");
+      cardImg.src = `${item.image}`;
+      cardImg.classList = "card-img";
+      card.appendChild(cardImg);
+
+      const contentDiv = document.createElement("div");
+      contentDiv.classList = "card-content";
+      card.appendChild(contentDiv);
+
+      const cardName = document.createElement("p");
+      cardName.classList = "name";
+      cardName.innerHTML = `${item.name}`;
+      contentDiv.appendChild(cardName);
+
+      // const episodeCard = document.createElement("button");
+      // episodeCard.innerHTML = `${item.episode}`;
+      // episodeCard.onclick((episode) => {
+      //   episode = `https://rickandmortyapi.com/api/episode/1+1`;
+      // });
+      // contentDiv.appendChild(episodeCard);
+
+      const bottomDiv = document.createElement("div");
+      bottomDiv.classList = "card-bottom";
+      contentDiv.appendChild(bottomDiv);
+
+      const statusCard = document.createElement("p");
+      statusCard.classList = "status";
+      statusCard.innerHTML = `${item.status}`;
+      bottomDiv.appendChild(statusCard);
+
+      const genderCard = document.createElement("p");
+      genderCard.classList = "gender";
+      genderCard.innerHTML = `${item.gender}`;
+      bottomDiv.appendChild(genderCard);
     });
 });
